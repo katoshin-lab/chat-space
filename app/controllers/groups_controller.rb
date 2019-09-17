fa15da7f720c172d338589be9b6091ec9e57b664
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-09-17 20:45:59 +0900

    Edit-controller-group-and-messages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,22 @@
 class GroupsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @group = Group.create(group_params)
+  end
+
+  def edit
+  end
+
+  def update
+    @group = Group.update(group_params)
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
 end
