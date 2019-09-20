3900934eda31f3cf1fa8811b83c6e2f80516f24d
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-09-20 19:59:46 +0900

    Add-message-M&C

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,14 @@
 class MessagesController < ApplicationController
 
   def index
-    @groups = Group.find(current_user.id)
+    @messages = Message.find(params[:group_id])
   end
 
   def create
+    @messages = Message.create(content: message_params[:content], image: message_params[:image], group_id: ,message_params[:group_id], user_id: current_user.id)
   end
 
+  private
+  def message_params
+    params.require(:message).permit(:content, :image, :group_id)
 end
