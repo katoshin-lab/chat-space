ae0fc42da95f98eb7878b811909f06b8a2b674aa
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-09-21 17:42:17 +0900

    Edit-message-haml&controller

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module ChatSpace
     end
     class Application < Rails::Application
       config.i18n.default_locale = :ja
+      config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
     end
   end
 end
