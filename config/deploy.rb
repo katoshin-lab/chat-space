9e33b1e06e05aa1f99be7c28719ed32b84546377
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-10-24 23:27:49 +0900

    edit-deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:katoshin-lab/chat-space.git"
+set :repo_url, "git@github.com:katoshin-lab/chat-space.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -12,7 +12,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/rails-chat-cpace.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.ssh/rails-chat-cpace.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
