935eea8664f69e148081db4a1dbb605f7da3756e
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-10-29 21:21:16 +0900

    detail

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fit: [800, 600]
+  process resize_to_fit: [800, 500]
   #
   # def scale(width, height)
   #   # do something
